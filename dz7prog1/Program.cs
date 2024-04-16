d9b4b63a42c55401454b868d7fa73a2bd916bd27
ModerrOn <sensansion@gmail.com>
2024-04-17 02:11:36 +0500

    create dzseminar 7 prog 1 program.cs

--- dz7prog1/Program.cs
+++ dz7prog1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+//в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+using System;
+
+void natureNum(int numM, int numN)
+{
+    if (numM - 1 == numN) return;
+    natureNum(numM, numN - 1);
+    Console.WriteLine($"{numN} ");
+}
+
+
+Console.WriteLine("Введите натуральное число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N, которое больше M");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= m) 
+{
+    Console.WriteLine($"Вывод всех натуральных чисел в промежутке от M до N, включая N: ");
+    natureNum(m, n); 
+} else
+{
+    Console.WriteLine("Вы ввели N < M. ");
+};
+Console.WriteLine("Программа завершена, для выхода нажмите любую клавишу...");
+Console.ReadLine();
